a7397c373a33a3122f591c6a63ac804d92bbf2a0
Niv <nivh@securingsam.com>
2021-11-10 00:18:12 +0200

    Added proper implementation files

--- src/Studio.cpp
+++ src/Studio.cpp
@@ -6,76 +6,62 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include "Studio.h"
 #include "Workout.h"
 #include "Trainer.h"
-#include "Action.h"
 
+void Studio::addWorkoutFromConfig(const std::string &workout_info, int workout_id)
+{
+    int first_delim_pos(workout_info.find(","));
+    int second_delim_pos(workout_info.find(",", first_delim_pos));
+    std::string workout_name(workout_info.substr(0, first_delim_pos));
+    std::string workout_type(first_delim_pos + 2, second_delim_pos);
+    WorkoutType w_type;
+    int price(0);
 
-class Studio {
-public:
-    Studio();
-    Studio(const std::string &configFilePath): open(false) {
-        bool num_trainers_read(false);
-        bool capacities_read(false);
-        int num_trainers(0);
-        int current_delim_pos(0);
-        std::fstream config_file;
-        std::string cur_line;
-        int workout_id(0);
-        config_file.open(configFilePath);
-
-        while (std::getline(config_file, cur_line)) {
-            if (cur_line.length() == 0 || cur_line.at(0) == '#') {
-                continue;
-            }
-            if (!num_trainers_read) {
-                num_trainers = std::stoi(cur_line);
-                num_trainers_read = true;
-            }
-            else if (!capacities_read) {
-                while (cur_line.find(",", current_delim_pos) != std::string::npos) {
-                    trainers.push_back(new Trainer(std::stoi(cur_line.substr(current_delim_pos,
-                                                                             cur_line.find(",", current_delim_pos)))));
-                    current_delim_pos = cur_line.find(",", current_delim_pos) + 1;
-                }
-                trainers.push_back(new Trainer(std::stoi(cur_line.substr(current_delim_pos))));
-                capacities_read = true;
-            }
-            else {
-                addWorkoutFromConfig(cur_line, workout_id);
-                workout_id++;
-            }
-        }
-    };
-    void start();
-    int getNumOfTrainers() const;
-    Trainer* getTrainer(int tid);
-    const std::vector<BaseAction*>& getActionsLog() const; // Return a reference to the history of actions
-    std::vector<Workout>& getWorkoutOptions();
-
-private:
-    bool open;
-    std::vector<Trainer*> trainers;
-    std::vector<Workout> workout_options;
-    std::vector<BaseAction*> actionsLog;
-
-    void addWorkoutFromConfig(const std::string &workout_info, int workout_id) {
-        int first_delim_pos(workout_info.find(","));
-        int second_delim_pos(workout_info.find(",", first_delim_pos));
-        std::string workout_name(workout_info.substr(0, first_delim_pos));
-        std::string workout_type(first_delim_pos + 2, second_delim_pos);
-        WorkoutType w_type;
-        int price(0);
-
-        if (workout_type.compare("Anaeorbic") == 0) {
-            w_type = ANAEROBIC;
-        } else if (workout_type.compare("Mixed") == 0) {
-            w_type = MIXED;
-        } else {
-            w_type = CARDIO;
-        }
-
-        price = std::stoi(workout_info.substr(second_delim_pos + 2));
-        workout_options.push_back(Workout(workout_id, workout_name, price, w_type));
+    if (workout_type.compare("Anaeorbic") == 0) {
+        w_type = ANAEROBIC;
+    } else if (workout_type.compare("Mixed") == 0) {
+        w_type = MIXED;
+    } else {
+        w_type = CARDIO;
     }
-};
+
+    price = std::stoi(workout_info.substr(second_delim_pos + 2));
+    workout_options.push_back(Workout(workout_id, workout_name, price, w_type));
+}
+
+Studio::Studio(const std::string &configFilePath): open(false)
+{
+    bool num_trainers_read(false);
+    bool capacities_read(false);
+    int num_trainers(0);
+    int current_delim_pos(0);
+    std::fstream config_file;
+    std::string cur_line;
+    int workout_id(0);
+    config_file.open(configFilePath);
+
+    while (std::getline(config_file, cur_line)) {
+        if (cur_line.length() == 0 || cur_line.at(0) == '#') {
+            continue;
+        }
+        if (!num_trainers_read) {
+            num_trainers = std::stoi(cur_line);
+            num_trainers_read = true;
+        }
+        else if (!capacities_read) {
+            while (cur_line.find(",", current_delim_pos) != std::string::npos) {
+                trainers.push_back(new Trainer(std::stoi(cur_line.substr(current_delim_pos,
+                                                                            cur_line.find(",", current_delim_pos)))));
+                current_delim_pos = cur_line.find(",", current_delim_pos) + 1;
+            }
+            trainers.push_back(new Trainer(std::stoi(cur_line.substr(current_delim_pos))));
+            capacities_read = true;
+        }
+        else {
+            addWorkoutFromConfig(cur_line, workout_id);
+            workout_id++;
+        }
+    }
+}
--- src/Workout.cpp
+++ src/Workout.cpp
@@ -2,22 +2,7 @@
 // Created by spl211 on 09/11/2021.
 //
 #include <string>
+#include "Workout.h"
 
-enum WorkoutType{
-    ANAEROBIC, MIXED, CARDIO
-};
-
-class Workout{
-public:
-    Workout(int w_id, std::string w_name, int w_price, WorkoutType w_type):
-        id(w_id), name(w_name), price(w_price), type(w_type) {};
-    int getId() const;
-    std::string getName() const;
-    int getPrice() const;
-    WorkoutType getType() const;
-private:
-    const int id;
-    const std::string name;
-    const int price;
-    const WorkoutType type;
-};
+Workout::Workout(int w_id, std::string w_name, int w_price, WorkoutType w_type):
+        id(w_id), name(w_name), price(w_price), type(w_type) {}
